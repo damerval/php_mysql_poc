0fe39f1b59f393c9d48a1e5810ccf1c986c6a657
damerval <damerval@gmail.com>
2016-07-15 15:07:53 -0800

    interim

--- grid_source.php
+++ grid_source.php
@@ -11,6 +11,9 @@ $b = $a * 2;
 $c = $b * 4;
 echo $c;
 */
+
+session_start();
+
 $servername = "localhost";
 $options = array("UID" => "sa", "PWD" => "7ofNine~", "Database" => "AdventureWorks2008R2");
 $conn = sqlsrv_connect($servername, $options);
@@ -19,9 +22,19 @@ if ($conn === false) {
   die (print_r("Cannot connect.", true));
 }
 
-$sql = "SELECT DatabaseLogID, PostTime, DatabaseUser, Event, [Schema], Object, TSQL FROM DatabaseLog WHERE Event LIKE ?";
-$criterion = "%TABLE%";
-$stmt = sqlsrv_query($conn, $sql, array($criterion));
+$sql = "select * from vDatabaseLog WHERE [Schema] = 'dbo' ";
+
+if (!empty($_SESSION)) {
+  switch ($_SESSION['user_id']) {
+    case 3: break;
+    case 2: $sql .= "OR Event = 'CREATE_TABLE' "; break;
+    case 1: $sql .= "OR Event like ? "; break;
+    default: $sql = "";
+  }
+} else $sql = "";
+
+$criterion1 = "%ALTER%";
+$stmt = sqlsrv_query($conn, $sql, array($criterion1));
 $data = array();
 
 while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
--- login.php
+++ login.php
@@ -6,7 +6,8 @@
  * Time: 11:09 AM
  */
 
-if (session_status() === PHP_SESSION_NONE){session_start();}
+
+session_start();
 $_SESSION = array();
 
 $form_submitted = $_SERVER['REQUEST_METHOD'] == 'POST';
@@ -17,7 +18,7 @@ $names = array(1 => "Philippe Damerval", 2 => "John Smith", 3 => "James Jones");
 if ($form_submitted) {
   $_SESSION['user_id'] = $_POST['sel_user'];
   $_SESSION['user_name'] = $names[$_POST['sel_user']];
-
+  header("Location: grid.html");
 }
 
 ?>
@@ -30,26 +31,25 @@ if ($form_submitted) {
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script type="text/javascript" language="javascript">
 
-    $(document).ready(function(){
-      <?php
-        $s = $_SESSION['user_id'] . " - " . $_SESSION['user_name'];
-        if ($form_submitted) { echo "alert(\"" . $s . "\");"; }
-      ?>
-      $("#but_submit").on('click', function() {
-        var selected_value = $("#sel_user").val();
-        var submit = selected_value != null;
-        if (submit) {
-          $("#fmain").submit();
-        } else alert ("You need to select a login before submitting.");
-      });
+  $(document).ready(function(){
+    <?php
+      if ($form_submitted) {
+        echo "alert(\"" . $_SESSION['user_id'] . " - " . $_SESSION['user_name'] . "\");";
+      }
+    ?>
+    $("#but_submit").on('click', function() {
+      if ($("#sel_user").val() != null) {
+        $("#fmain").submit();
+      } else alert ("You need to select a login before submitting.");
     });
+  });
 
   </script>
 </head>
 
 <body>
 
-<form name="fmain" id="fmain" action="" method="post" onsubmit="alert($('#sel_user').val(); event.preventDefault();">
+<form name="fmain" id="fmain" method="post" onsubmit="alert($('#sel_user').val(); event.preventDefault();">
 
   <select title="User Selector" id="sel_user" name="sel_user">
     <option disabled selected>Choose login</option>
